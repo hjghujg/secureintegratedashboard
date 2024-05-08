fc7ac5ad45c7cb0250eada10ddf34b003a41383d
hjghujg <ksjcelvinjr1993@hotmail.com>
2024-05-09 03:41:50 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// Alice should have the initial balance minus tranfer amount
-if (aliceBalanceAfter[0].amount !== ALICE_INITIAL_BALANCE - TRANSFER_AMOUNT)
-  throw new Error("Alice's balance after transfer is incorrect");
-
-// Sponsor should have the initial balance minus gas
-if (sponsorBalanceAfter[0].amount >= SPONSOR_INITIAL_BALANCE)
-  throw new Error("Sponsor's balance after transfer is incorrect");
+function longestConsecutive(nums) {
+  const set = new Set(nums);
+  let longest = 0;
+  for (const num of set) {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+      while (set.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+      longest = Math.max(longest, currentStreak);
+    }
+  }
+  return longest;
+}
